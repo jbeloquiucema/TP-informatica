b7f7b0c9126d9664a60929dc166b7e4ad9d24738
jbeloquiucema <134952934+jbeloquiucema@users.noreply.github.com>
2023-06-14 15:44:17 -0300

    Update game_controller_poo.py

--- game_controller_poo.py
+++ game_controller_poo.py
@@ -1,71 +1,71 @@
 from db_games import get_db
-from clase_games import Game
+from clase_remeras import Remera
 
 
-def insert_game(game_code, name, num_players, price, min_age, category, platform):
+def insert_remera(remera_code, color, tamaño_letra, price, cantidad, frase, talle):
     db = get_db()
     cursor = db.cursor()
-    statement = "INSERT INTO games (game_code, name, num_players, price, min_age, category, platform) \
+    statement = "INSERT INTO remeras (remera_code, color, tamaño_letra, price, cantidad, frase, talle) \
     VALUES ( ?, ?, ?, ? ,?, ?, ?)"
-    cursor.execute(statement, [game_code, name, num_players, price, min_age, category, platform])
+    cursor.execute(statement, [remera_code, color, tamaño_letra, price, cantidad, frase, talle])
     db.commit()
     return True
 
-def update_game(game_code, name, num_players, price, min_age, category, platform):
+def update_remera(remera_code, color, tamaño_letra, price, cantidad, frase, talle):
     db = get_db()
     cursor = db.cursor()
-    statement = "UPDATE games SET name = ?, num_players = ?, min_age= ?, category= ?, platform= ? \
-    WHERE game_code = ?"
-    cursor.execute(statement, [name, num_players, price, min_age, category, platform, game_code])
-    db.commit()
-    return True
-
-
-def delete_game(game_code):
-    db = get_db()
-    cursor = db.cursor()
-    statement = "DELETE FROM games WHERE game_code = ?"
-    cursor.execute(statement, [game_code])
+    statement = "UPDATE remeras SET color = ?, tamaño_letra = ?, cantidad= ?, frase= ?, talle= ? \
+    WHERE remera_code = ?"
+    cursor.execute(statement, [color, tamaño_letra, price, cantidad, frase, talle, remera_code])
     db.commit()
     return True
 
 
-def get_by_id(game_code):
+def delete_remera(remera_code):
     db = get_db()
     cursor = db.cursor()
-    statement = "SELECT game_code, name, num_players, price, min_age, category, platform FROM games \
-    WHERE game_code = ?"
-    cursor.execute(statement, [game_code])
-    single_game = cursor.fetchone()
-    game_code = single_game[0]
-    name = single_game[1]
-    num_players = single_game[2]
-    price = single_game[3]
-    min_age = single_game[4]
-    category = single_game[5]
-    platform = single_game[6]
-    game = Game (game_code, name, num_players, price, min_age, category, platform) 
+    statement = "DELETE FROM remeras WHERE remera_code = ?"
+    cursor.execute(statement, [remera_code])
+    db.commit()
+    return True
+
+
+def get_by_id(remera_code):
+    db = get_db()
+    cursor = db.cursor()
+    statement = "SELECT remera_code, color, tamalo_letra, price, cantidad, frase, talle FROM remeras \
+    WHERE remera_code = ?"
+    cursor.execute(statement, [remera_code])
+    single_remera = cursor.fetchone()
+    remera_code = single_remera[0]
+    color = single_remera[1]
+    tamaño_letra = single_remera[2]
+    price = single_remera[3]
+    cantidad = single_remera[4]
+    frase = single_remera[5]
+    talle = single_remera[6]
+    remera = Remera (remera_code, color, tamaño_letra, price, cantidad, frase, talle) 
     return game.serialize_details()
 
 
-def get_games():
+def get_remeras():
     db = get_db()
     cursor = db.cursor()
-    query = "SELECT game_code, name, num_players, price, min_age, category, platform FROM games"
+    query = "SELECT remera_code, color, tamaño_letra, price, cantidad, frase, talle FROM remeras"
     cursor.execute(query)
-    game_list = cursor.fetchall()
-    list_of_games=[]
-    for game in game_list:
-        game_code = game[0]
-        name = game[1]
-        num_players = game[2]
-        price = game[3]
-        min_age = game[4]
-        category = game[5]
-        platform = game[6]
-        game_to_add = Game(game_code, name, num_players, price, min_age, category, platform)
-        list_of_games.append(game_to_add)
-    return list_of_games
+    remera_list = cursor.fetchall()
+    list_of_remeras=[]
+    for remera in remera_list:
+        remera_code = remera[0]
+        color = remera[1]
+        tamaño_letra = remera[2]
+        price = remera[3]
+        cantidad = remera[4]
+        frase = remera[5]
+        talle = remera[6]
+        remera_to_add = Remera(remera_code, color, tamaño_letra, price, cantidad, frase, talle)
+        list_of_remeras.append(remera_to_add)
+    return list_of_remeras
 
 
 
