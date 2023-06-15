1e05d448bc9ee8f8dece51fd4126de32ecf11ef9
jbeloquiucema <134952934+jbeloquiucema@users.noreply.github.com>
2023-06-15 14:02:07 -0300

    archivo.csv.py

--- csv.py
+++ csv.py
@@ -1,4 +1,3 @@
-#Este es el link de nuestro collab donde aparece nuestra dataframe con todas las remeras (con su descripicion adecuada) que nos compran.
+#Este es el link de nuestro collab donde aparece nuestra dataframe con todas las remeras(con su descripicion adecuada) que nos compran. También se puede observar un grafico de barras (con la cantidad de pedidos de cada color), y un grafico de torta (donde se puede observar el porcentage de cantidad de remeras por color).
 
-
-https://colab.research.google.com/drive/1643KY59HBxtByacDpkLRf4S9zeZAy6ju?usp=sharing
+https://colab.research.google.com/drive/15eSKaIk_lMQyJe0RVaQdgF76u4UM07iY?usp=sharing
