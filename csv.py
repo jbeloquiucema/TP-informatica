0a11272b790fcadfc8a37844c49213662da9510d
jbeloquiucema <134952934+jbeloquiucema@users.noreply.github.com>
2023-06-15 11:52:21 -0300

    Update csv.py

--- csv.py
+++ csv.py
@@ -1,3 +1,4 @@
 #Este es el link de nuestro collab donde aparece nuestra dataframe con todas las remeras (con su descripicion adecuada) que nos compran.
 
-https://colab.research.google.com/drive/1643KY59HBxtByacDpkLRf4S9zeZAy6ju#scrollTo=zlQK_CPpZjyt
+
+https://colab.research.google.com/drive/1643KY59HBxtByacDpkLRf4S9zeZAy6ju?usp=sharing
